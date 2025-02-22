4519ddfaac521eff79c137c78be92fbaa43730a2
codepall <minecraftnfindo@gmail.com>
2025-02-22 19:11:40 +0700

    Update Senin.jsx

--- src/components/Mapel/Senin.jsx
+++ src/components/Mapel/Senin.jsx
@@ -6,7 +6,7 @@ const Senin = () => {
 					<div className="flex justify-center items-center text-sm">08.05-09.25</div>
 				</div>
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">PPkN</div>
+					<div className="w-[50%] text-base  font-medium">PJOK</div>
 					<div className="flex justify-center items-center text-sm">09.25-10.05</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
@@ -14,12 +14,12 @@ const Senin = () => {
 					<div className="flex justify-center items-center text-sm">10.05-10.35</div>
 				</div>
 				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-					<div className="w-[50%] text-base  font-medium">PPkN</div>
+					<div className="w-[50%] text-base  font-medium">PJOK</div>
 					<div className="flex justify-center items-center text-sm">10.35-11.50</div>
 				</div>
 
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
-					<div className="w-[50%] text-base  font-medium">Bahasa Indonesia</div>
+					<div className="w-[50%] text-base  font-medium">Matematika</div>
 					<div className="flex justify-center items-center text-sm">11.50-13.00</div>
 				</div>
         </>
