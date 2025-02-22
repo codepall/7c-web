d1f344b840a4cf9ed00245764cfa98dc63f4204a
codepall <minecraftnfindo@gmail.com>
2025-02-22 19:20:26 +0700

    Update Jumat.jsx

--- src/components/Mapel/Jumat.jsx
+++ src/components/Mapel/Jumat.jsx
@@ -5,7 +5,7 @@ const Jumat = () => {
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">IPA</div>
+				<div className="w-[50%] text-base  font-medium">Informatika</div>
 				<div className="flex justify-center items-center text-sm">07.25-08.45</div>
 			</div>
 			<div
@@ -19,7 +19,7 @@ const Jumat = () => {
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="700">
-				<div className="w-[50%] text-base  font-medium">IPA</div>
+				<div className="w-[50%] text-base  font-medium">Informatika</div>
 				<div className="flex justify-center items-center text-sm">09.15-09.50</div>
 			</div>
 			<div
