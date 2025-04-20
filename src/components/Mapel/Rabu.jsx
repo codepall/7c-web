28d7923f9e79f68781d9243fa03a97742c0df1ed
codepall <minecraftnfindo@gmail.com>
2025-04-20 21:04:30 +0700

    Update Rabu.jsx

--- src/components/Mapel/Rabu.jsx
+++ src/components/Mapel/Rabu.jsx
@@ -2,23 +2,23 @@ const Rabu = () => {
     return (
         <>
         	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Bahasa Inggris</div>
-					<div className="flex justify-center items-center text-sm">07.40-08.50</div>
+					<div className="w-[50%] text-base  font-medium">Seni Budaya</div>
+					<div className="flex justify-center items-center text-sm">07.40-09.25</div>
 				</div>
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
 					<div className="w-[50%] text-base  font-medium">Bahasa Indonesia</div>
-					<div className="flex justify-center items-center text-sm">08.50-10.00</div>
+					<div className="flex justify-center items-center text-sm">09.25-10.00</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
 					<div className="w-[50%] text-base  font-medium">Istirahat</div>
 					<div className="flex justify-center items-center text-sm">10.00-10.30</div>
 				</div>
 				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-					<div className="w-[50%] text-base  font-medium">Bimbingan Konseling</div>
+					<div className="w-[50%] text-base  font-medium">Bahasa Indonesia</div>
 					<div className="flex justify-center items-center text-sm">10.30-11.05</div>
 				</div>
 				<div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
-					<div className="w-[50%] text-base  font-medium">PPkN</div>
+					<div className="w-[50%] text-base  font-medium">Informatika</div>
 					<div className="flex justify-center items-center text-sm">11.05-13.00</div>
 				</div>
 			
