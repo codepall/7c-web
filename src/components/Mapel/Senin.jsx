1cbf19c9f3939a3a1f330726599afe1279cc7e15
codepall <minecraftnfindo@gmail.com>
2025-04-20 20:58:46 +0700

    Update Senin.jsx

--- src/components/Mapel/Senin.jsx
+++ src/components/Mapel/Senin.jsx
@@ -2,11 +2,11 @@ const Senin = () => {
     return (
         <>
         	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Bahasa Inggris</div>
+					<div className="w-[50%] text-base  font-medium">Bahasa Jawa</div>
 					<div className="flex justify-center items-center text-sm">08.05-09.25</div>
 				</div>
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">PJOK</div>
+					<div className="w-[50%] text-base  font-medium">IPS</div>
 					<div className="flex justify-center items-center text-sm">09.25-10.05</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
@@ -14,12 +14,17 @@ const Senin = () => {
 					<div className="flex justify-center items-center text-sm">10.05-10.35</div>
 				</div>
 				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-					<div className="w-[50%] text-base  font-medium">PJOK</div>
-					<div className="flex justify-center items-center text-sm">10.35-11.50</div>
+					<div className="w-[50%] text-base  font-medium">IPS</div>
+					<div className="flex justify-center items-center text-sm">10.35-11.15</div>
 				</div>
 
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
-					<div className="w-[50%] text-base  font-medium">Matematika</div>
+					<div className="w-[50%] text-base  font-medium">Bimbingan Konseling</div>
+					<div className="flex justify-center items-center text-sm">11.15-11.50</div>
+				</div>
+
+		                <div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+					<div className="w-[50%] text-base  font-medium">Bahasa Indonesia</div>
 					<div className="flex justify-center items-center text-sm">11.50-13.00</div>
 				</div>
         </>
