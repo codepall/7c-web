c7ce719cc26257c393477f0c496ac632c93021a6
codepall <minecraftnfindo@gmail.com>
2025-04-20 21:06:48 +0700

    Update Kamis.jsx

--- src/components/Mapel/Kamis.jsx
+++ src/components/Mapel/Kamis.jsx
@@ -12,7 +12,7 @@ const Kamis = () => {
 				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">PAI</div>
+				<div className="w-[50%] text-base  font-medium">Matematika</div>
 				<div className="flex justify-center items-center text-sm">08.50-10.00</div>
 			</div>
 			<div
@@ -26,15 +26,15 @@ const Kamis = () => {
 				className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="800">
-				<div className="w-[50%] text-base  font-medium">PAI</div>
-				<div className="flex justify-center items-center text-sm">10.30-11.05</div>
+				<div className="w-[50%] text-base  font-medium">Bahasa Inggris</div>
+				<div className="flex justify-center items-center text-sm">10.30-11.40</div>
 			</div>
 			<div
 				className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="800">
-				<div className="w-[50%] text-base  font-medium">Seni Budaya</div>
-				<div className="flex justify-center items-center text-sm">11.05-13.00</div>
+				<div className="w-[50%] text-base  font-medium">Bahasa Indonesia</div>
+				<div className="flex justify-center items-center text-sm">11.40-13.00</div>
 			</div>
 		</>
 	)
