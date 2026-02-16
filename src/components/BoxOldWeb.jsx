00c90e9bc6a78ba25d6fecacd02e4317d07394b3
codepall <naufalindo79@gmail.com>
2026-02-17 05:19:29 +0700

    Update BoxOldWeb.jsx

--- src/components/BoxOldWeb.jsx
+++ src/components/BoxOldWeb.jsx
@@ -2,7 +2,7 @@
 const BoxOldWeb = () => {
 	return (
 		<div id="BoxOldWeb">
-			<a href="https://7c-site.github.io/">
+			<a href="https://codepall.github.io/7c/">
 				<div className="flex justify-between">
 					<img src="/link.png" alt="" className="w-auto h-6" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
